5d04971b3ad9493a5d483f2fefc1dfdb211acecf
teeranardsap <teeranard.sap@student.mahidol.edu>
2026-08-27 15:50:21 +0700

    Switch to threading

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 # สถานะของเกม
 game_results = []
@@ -58,4 +58,4 @@ def handle_reset():
     emit('game_reset', broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
